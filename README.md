d9bdb47d5b88ec3cdbf647d9c81daf1c5e159e4f
replicatingTrouts <replicatingTrouts@gmail.com>
2022-07-18 18:47:06 -0500

    re-re-re-initializing repo. Man you'd think I'd have this figured out by now.

---
+++ README.md
@@ -0,0 +1,156 @@
+Plex Sub Downloader
+===================
+
+
+What is it?
+-----------
+
+This is a command-line tool designed to automate the downloading of subtitles for media on your [Plex Media Server](https://www.plex.tv/). It makes use of [Flask](https://flask.palletsprojects.com/en/2.1.x/) and [Python-PlexAPI](https://github.com/pkkid/python-plexapi) to listen for newly-added media, and [Subliminal](https://github.com/Diaoul/subliminal) to search your favorite subtitle providers.
+
+Okay, Cool, but Why?
+--------------------
+
+Plex has built-in Agents for downloading subtitles from OpenSubtitles.org, but it doesn't search for subtitles automatically, and, more importantly, doesn't support VIP accounts (which means you're stuck reading ads _in your subtitles!_).
+
+And there's other tools like [Sub-Zero](https://github.com/pannal/Sub-Zero.bundle) and [Bazarr](https://github.com/bazarr/), but these work best if you've already bought into the [Sonarr](https://sonarr.tv/)/[Radarr](https://radarr.video/) ecosystem. And, honestly, while these tools are great, I find them to be over-built for what I want to do.
+
+I just wanted something that tries to download subtitles for new media added to my Plex server, and that's it.
+
+---
+Requirements
+------------
+---
+- Requires python >=3.6
+- You'll need to purchase [Plex Pass](https://www.plex.tv/plex-pass/) to enable [webhooks](https://support.plex.tv/articles/115002267687-webhooks/) 
+
+---
+Setup
+-----
+---
+### NOTE: This project is still VERY MUCH a work in progress. The setup process will hopefully be easier in the next release. ###
+-----------------
+
+
+- First, install plex_sub_downloader:
+```
+pip3 install plex_sub_downloader
+```
+- Then, find an auth token for your Plex account:
+  https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
+
+
+- Then, create a config.json file somewhere:
+
+```
+{
+    "plex_base_url": "<url to your plex server, ie http://localhost:32400>",
+    "plex_auth_token": "<your auth token here>",
+    "subliminal": {
+        "languages": [
+            "eng"
+        ]
+    },
+    "webhook_host": "0.0.0.0",
+    "webhook_port": <some port number, default = 5000>
+}
+```
+
+- Next, run `configtest` on this config file to make sure it's formatted correctly
+```
+plex_sub_downlaoder --config path/to/config.json configtest
+```
+
+You should get a result like:
+```
+2022-07-16 21:08:38:plex_sub_downloader:INFO - Testing config file '/path/to/config.json'
+2022-07-16 21:08:38:plex_sub_downloader:INFO - config file is valid.
+```
+
+- Next, we need to get the url to add to Plex. Run plex_sub_downloader with the `start-webhook` flag: 
+
+```
+plex_sub_downloader --config path/to/config.json start-webhook --debug
+``` 
+
+Assuming it starts and runs correctly, the last few lines should look like
+```
+ * Serving Flask app 'plex_sub_downloader.plex_sub_downloader' (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: off
+ * Running on all addresses.
+   WARNING: This is a development server. Do not use it in a production deployment.
+ * Running on http://<ip address>:<port>/ (Press CTRL+C to quit)
+ ```
+
+The url you'll need to add to Plex will be `http://<ip address>:<port>/webhook`. 
+
+- Open Plex, navigate to Settings, and select Webhooks from the left-hand menu. Add your webhook url.
+
+- To verify that Plex can call your webhook, start playing a video. You'll get a big dump of data, starting with:
+```
+2022-07-16 21:28:14:PlexSubDownloader:DEBUG - handleWebhookEvent
+2022-07-16 21:28:14:PlexSubDownloader:DEBUG - Event type: media.play
+<some ip addr> - - [16/Jul/2022 21:28:14] "POST /webhook HTTP/1.1" 200 -
+```
+
+Congrats! It's probably working?
+
+---
+Command-line Arguments
+----------------------
+---
+| Argument | Description |
+| -------- | ----------- |
+| -h, --help | show this help message and exit |
+| -c CONFIG, --config CONFIG | Config File |
+| -d, --debug | Enable debug logging |
+| configtest | Run validation on config file |
+| start-webhook| Run http webhook server |
+
+
+---
+Configuration
+-------------
+---
+
+| Parameter | Required? | Description |
+| --------- | --------- | ----------- |
+| plex_base_url | Yes |Base url to reach your Plex Media Server (ie `"http://127.0.0.1:32400"`) |
+| plex_auth_token | Yes |Authentication token, needed to send requests to your server. |
+| webhook_host | optional, default `"127.0.0.1"` | the hostname to listen on. By default, the server will only be accessible from the computer running it. Set this to `"0.0.0.0"` to make it publicly available on your network.|
+| webhook_port | Optional, default `5000` | the port to listen on. |
+| subtitle_destination | Optional, default `"with_media"` | Either `"with_media"` or `"metadata"`. `"with_media"` will save subtitle files alongside the media files. `"metadata"` will upload the subtitles to Plex, which stores the subtitles as part of the media's metadata. If Plex and PlexSubDownloader don't run on the same server, you'll need to set this to `"metadata"`.
+| languages | Optional, default `["en"]` | Array of [IETF language tags](https://en.wikipedia.org/wiki/IETF_language_tag#List_of_common_primary_language_subtags) to download subtitles for.|
+| subtitle_providers | Optional, defaults to all | List of subtitle providers to search. Currently, Subliminal supports `"addic7ed", "legendastv", "opensubtitles", "opensubtitlesvip", "podnapisi", "shooter", "thesubdb", "tvsubtitles"`. |
+|subtitle_provider_configs | Optional, default None | Dictionary of configuration parameters for your chosen subtitle providers. Each provider may support different config parameters. See [Subliminal's documentation](https://subliminal.readthedocs.io/en/latest/api/providers.html) for more details. |
+| log_level | Optional, default logging.INFO | log level. Expects an integer value. 
+
+
+### Example configuration:
+
+```
+{
+    "plex_base_url": "http://127.0.0.1:32400",
+    "plex_auth_token": "<token-goes-here>"
+    "webhook_host": "0.0.0.0",
+    "webhook_port": 6669,
+    "subtitle_destination": "metadata",
+    "languages": [
+        "eng",
+        "es"
+    ],
+    "subtitle_providers": [
+        "opensubtitlesvip",
+        "tvsubtitles"
+    ],
+    "subtitle_provider_configs": {
+        "opensubtitlesvip": {
+            "username": "<username here>",
+            "password": "<password here>"
+        }
+    },
+    "log_level": 20
+}
+```
---
+++ plex_sub_downloader/PlexSubDownloader.py
@@ -0,0 +1,177 @@
+import os
+import tempfile
+from plex_sub_downloader.subliminalHelper import SubliminalHelper
+from subliminal.video import Video as SubVideo
+from subliminal.subtitle import Subtitle
+from plex_sub_downloader.PlexWebhookEvent import PlexWebhookEvent
+from plex_sub_downloader.logger import Logger
+from plexapi.server import PlexServer
+from plexapi.video import Video
+from plexapi.library import LibrarySection
+from plexapi.media import SubtitleStream
+
+log = Logger.getInstance().getLogger()
+
+class PlexSubDownloader:
+
+    def __init__(self):
+        self.sub = None
+        self.plex = None
+
+    def configure(self, config):
+        """initializes and configures the needed classes for PlexSubDownloader to work.
+        :param object config: config json. See config.schema.json for structure.
+        :return: True if everything initializes correctly, otherwise False.
+        """
+
+        log.info("Configuring PlexSubDownloader")
+        self.config = config
+        self.subtitle_destination = config.get('subtitle_destination', 'with_media')
+        self.sub = SubliminalHelper(languages=config.get('languages', None), 
+        providers= config.get('subtitle_providers', None),
+        provider_configs=config.get('subtitle_provider_configs', None))
+        
+        self.plexServer = PlexServer(baseurl=config['plex_base_url'], token=config['plex_auth_token'])
+        
+        if config['subtitle_destination'] == 'with_media' and self.checkLibraryPermissions() == False:
+            log.error("One or more of the Plex libraries are not readable/writable by the current user.")
+            return False
+        return True
+        
+
+    def handleWebhookEvent(self, event):
+        """Handles the given webhook event. If the event is of type "library.new", it will start
+        the process of downloading subtitles.
+        :param PlexWebhookEvent event:
+        """
+        log.debug("handleWebhookEvent")
+        log.debug("Event type: " + event.event)
+
+        if event.event == "library.new":
+            log.info("Handling library.new event")
+            log.info(f'Title: {event.Metadata.title}, type: {event.Metadata.type}, section: {event.Metadata.librarySectionTitle}')
+            
+            video = self.plexServer.fetchItem(ekey=event.Metadata.key)
+            video.reload()
+
+            missingVideos = self.getVidsMissingSubtitles([video])
+            log.info("Found " + str(len(missingVideos)) + " videos missing subtitles")
+            log.info([f'{video.title}, {video.key}' for video in missingVideos])
+            if len(missingVideos) > 0:
+                subtitles = self.downloadSubtitlesForVideos(missingVideos)
+
+                if self.subtitle_destination == "metadata":
+                    self.uploadSubtitlesToMetadata(missingVideos, subtitles)
+                else:
+                    self.sub.save_subtitles(subtitles)
+            else:
+                log.info("No subtitles to download, doing nothing!")
+
+            
+    def getVidsMissingSubtitles(self,videos):
+        """Search the given list of videos for ones that don't already have subtitles.
+        For videos of type 'season' or 'show', this will search through all of the episodes
+        as well.
+        :param list videos: list of plexapi.video.Video objects.
+        :return: list of Video objects that don't have any subtitles.
+        """
+
+        vidsMissingSubs = []
+        for v in videos:
+            if v.type == 'movie' or v.type == 'episode':
+                if self.checkVideoForSubtitles(v) == False:
+                    vidsMissingSubs.append(v)
+                
+            elif v.type == 'season' or v.type == 'show':
+                eps = v.episodes()
+                for e in eps:
+                    e.reload()
+                    if self.checkVideoForSubtitles(e) == False:
+                        vidsMissingSubs.append(e)
+
+        return vidsMissingSubs
+
+    def checkVideoForSubtitles(self, video):
+        """Checks the given video for subtitles by retrieving the SubtitleStreams.
+        Checks against the list of languages provided in config['subliminal']['languages']. If _any_ 
+        language isn't found, this will return False.
+        :param video: plexapi.video.Video object
+        :return: boolean, True if the video has subtitles for every requested language, False otherwise
+        """
+        
+        languagesNotFound = self.config['subliminal']['languages']
+
+        subs = video.subtitleStreams()
+        log.debug(f'Found {len(subs)} subtitles for video {video.title} {video.key}')
+        for sub in subs:
+            log.debug(f'subtitle {sub.displayTitle} language code:{sub.languageCode}, format: {sub.format}, forced: {sub.forced}')
+            if sub.languageCode in languagesNotFound:
+                languagesNotFound.remove(sub.languageCode)
+        
+        return len(languagesNotFound) == 0
+        
+    def downloadSubtitlesForVideos(self, videos):
+        """Attempts to download subtitles for the given list of videos.
+        :param list videos: list of plexapi.video.Video objects.
+        :return: dict[subliminal.video.Video, list[subliminal.subtitle.Subtitle]]
+        """
+
+        log.info("Downloading subtitles for " + str(len(videos)) + " videos")
+        log.info([video.title for video in videos])
+        subtitles = self.sub.search_videos(videos)
+        return subtitles
+
+
+    def uploadSubtitlesToMetadata(self, plexVideos, subtitles):
+        """Saves the subtitles to Plex.
+        :param list videos: list of plexapi.video.Video objects.
+        :param dict subtitles: dict of dict[subliminal.video.Video, list[subliminal.subtitle.Subtitle]]
+        """
+
+        log.info("Saving subtitles to Plex metadata")
+        tempdir = tempfile.gettempdir()
+        for video in plexVideos:
+            filepath = video.media[0].parts[0].file
+            for subVideo, subtitles in subtitles.items():
+                if subVideo.name == filepath:
+                    log.info(f'found {len(subtitles)} for video {subVideo.name}')
+
+                    savedSubtitlePaths = self.sub.save_subtitle(subVideo, subtitles, destination=tempdir)
+                    for subtitlePath in savedSubtitlePaths:
+                        log.debug(f'Uploading subtitles \'{subtitlePath}\' to video {video.title} {video.key}')
+                        video.uploadSubtitles(subtitlePath)
+
+
+    def checkLibraryPermissions(self, sectionId=None):
+        """Checks whether the application has permissions to read/write to the base paths of each section 
+        within Plex's library.
+        :param string sectionId: An optional id value to just check permissions of a single section.
+        :return: True if all sections are read/writeable, otherwise False.
+        """
+
+        log.debug("Checking library permissions")
+        sections = []
+
+        checkedOk = True
+        if sectionId != None:
+            sections = [self.plexServer.library.sectionByID(sectionId)]
+        else:
+            sections = self.plexServer.library.sections()
+
+        for section in sections:
+            locations = section.locations
+            for location in locations:
+                exists = os.path.exists(location)
+                if not exists:
+                    log.error(f'Error checking library permissions. Directory \'{location}\' doesnt exist?')
+                    checkedOk = False
+                else:
+                    read_access = os.access(location, os.R_OK)
+                    write_access = os.access(location, os.W_OK)
+                    if not read_access or not write_access:
+                        log.error(f'Error checking library permissions. Cannot read/write to directory \'{location}\'')
+                        checkedOk = False
+        
+        return checkedOk
+
+
